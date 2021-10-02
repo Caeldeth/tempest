3502e2f7f3be5911b4206fe886ec04213419b006
Caeldeth <sabrael@hybrasyl.com>
2021-10-02 17:56:43 -0500

    get initial api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,32 +10,86 @@ var apiLang='&lang=en';
 // shortcut for getting weather icon
 var weatherIcon='http://openweathermap.org/img/wn/';
 // url for current weather call to get lat/lon
-var geoCall='http://api.opneweathermap.org/data/weather?q='
+var geoCall='http://api.openweathermap.org/data/2.5/weather?q='
 // create search history array
 var searchHist = [];
 
 // page load, here we go
 $(document).ready(function(){
+    // hide empty grid pieces
     $('#weather-grid').addClass('invisible');
     $('#history-grid').addClass('invisible');
     $('#city-grid').addClass('invisible');
-    $('#error-msg').innerHtml('');
-    showHistory();
+    // reset error message
+    $('#error-msg').text('');
+    //show history if any
+    // showHistory();
 });
 
-
+// search button handling
 $('#search-button').click(function(){
-    if (citySearch = null) {
-        $('#weather-grid').removeClass('invisible');
-        $('#history-grid').removeClass('invisible');
-        $('#city-grid').removeClass('invisible');
-        $('#error-msg').innerHtml('');
-        citySearch = $("#search-input")
-            .val()
-            .trim(); 
+    citySearch = $("#search-input")
+    .val().toLowerCase()
+    .trim();
+    console.log("City search is: " + citySearch)
+
+    if (citySearch != null) {
+    //    $('#weather-grid').removeClass('invisible');
+    //    $('#history-grid').removeClass('invisible');
+    //    $('#city-grid').removeClass('invisible');
+    //    $('#error-msg').text('');
+        
+        getWeather(citySearch);
     } else {
         $('#error-msg').text('Please enter a valid city.');
         $('#error-msg').css('color','red');
     }
 })
 
+function getWeather(search) {
+    var callWeatherUrl = 
+        geoCall
+        + search
+        + apiKey
+        + apiUnits
+        + apiLang;
+    console.log(callWeatherUrl);
+
+    $.ajax({
+        url: callWeatherUrl,
+        method: 'GET',
+        statusCode: {
+            404: function() {
+                $('#error-msg').text('City not found!  Please try again.');
+                $('#error-msg').css('color','red');
+            }
+        }
+    }).then(function(response){
+        $('#error-msg').text('');
+        $('#city-grid').removeClass('invisible');
+        $('#deets-holder').removeClass('invisible');
+        
+        var results = response;
+        var name = results.name;
+        var temperature = Math.floor(results.main.temp);
+        var humidity = results.main.humidity;
+        var windSpeed = results.wind.speed;
+        var currDate = new Date(results.dt * 1000).toLocaleDateString('en-US');
+        var icon = results.weather[0].icon;
+        var iconPath = weatherIcon + icon +'@2x.png';
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        var country = response.sys.country;
+
+        $('#city-name').text(name + " (" + currDate + ") "  );
+        $('#city-icon').attr('src', iconPath);
+        $('#city-loc').html(country + ", Lat: " + lat + ", Lon: " + lon);
+        $('#temp-result').html("Temperature: " + temperature);
+        $('#wind-result').html("Wind: " + windSpeed);
+        $('#humidity-result').text("Humidity: " + humidity);
+
+        console.log(iconPath);
+    });
+
+
+}
