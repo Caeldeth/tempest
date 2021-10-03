52d89491f1686b27e5c164392af81a5fcf00758f
Caeldeth <sabrael@hybrasyl.com>
2021-10-02 22:06:45 -0500

    correct typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,11 +62,11 @@ $('#jumbotron').click(function(){
     // string the country, lat and lon together
     $('#city-loc').html("country" + ", Lat: " + "lat" + ", Lon: " + "lon");
     // show the temperature
-    $('#temp-result').html("Temperature: " + "temperature" + ' °F');
+    $('#temp-result').html("Temperature: " + "temperature" + " °F");
     // show the wind speed
-    $('#wind-result').html("Wind: " + "windSpeed" + ' MPG');
+    $('#wind-result').html("Wind: " + "windSpeed" + " MPH");
     // show the humidity
-    $('#humidity-result').text("Humidity: " + "humidity" +'%');
+    $('#humidity-result').text("Humidity: " + "humidity" + "%");
     $('#uv-result').html("UV Index: " + 
     '<span class="badge badge-pill badge-light" id="uv-color">'
     + "uvi"
