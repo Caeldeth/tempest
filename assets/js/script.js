2883afa5a7256688cdb809e48720188f29197dc5
Caeldeth <sabrael@hybrasyl.com>
2021-10-02 22:09:51 -0500

    actually add things to the right spot' exit end
    top
    stop
    '

--- assets/js/script.js
+++ assets/js/script.js
@@ -216,11 +216,11 @@ function getWeather(search) {
         // string the country, lat and lon together
         $('#city-loc').html(country + ", Lat: " + lat + ", Lon: " + lon);
         // show the temperature
-        $('#temp-result').html("Temperature: " + temperature);
+        $('#temp-result').html("Temperature: " + temperature + " °F");
         // show the wind speed
-        $('#wind-result').html("Wind: " + windSpeed);
+        $('#wind-result').html("Wind: " + windSpeed) + " MPH";
         // show the humidity
-        $('#humidity-result').text("Humidity: " + humidity);
+        $('#humidity-result').text("Humidity: " + humidity + "%");
 
     });
 }
